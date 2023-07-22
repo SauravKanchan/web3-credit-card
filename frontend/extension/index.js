524f766a69473ee1c0d1cb56bf93206a4334bbc1
SauravKanchan <sauravnk30@gmail.com>
2023-07-22 11:56:49 +0200

    able to inject code to the page

--- frontend/extension/index.js
+++ frontend/extension/index.js
@@ -17,35 +17,8 @@ const auth = new AuthProvider(localAppAddress, {
 });
 
 (async () => {
-  let provider;
-  function script() {
-    window.addEventListener(
-      "message",
-      function (event) {
-        if (event.data.type === "FROM_MY_EXTENSIONS") {
-          window.ethereum = event.data.value;
-          console.log("window.etherum", window.ethereum);
-        }
-      },
-      false
-    );
-  }
-
-  function inject(fn) {
-    const script = document.createElement("script");
-    script.text = `(${fn.toString()})();`;
-    document.documentElement.appendChild(script);
-  }
-
-  inject(script);
-  try {
-    await auth.init();
-    provider = await auth.loginWithSocial("google");
-    console.log("provider", provider);
-    console.log("login", await auth.isLoggedIn());
-    // await auth.showWallet();
-    window.postMessage({ type: "FROM_MY_EXTENSIONS", text: provider }, "*");
-  } catch (e) {
-    alert(e);
-  }
+  let provider = await auth.init();
+  await auth.loginWithSocial("google");
+  await auth.isLoggedIn();
+  window.ethereum = provider;
 })();
---
+++ frontend/extension/popup.js
@@ -0,0 +1,10 @@
+function loadScript(scriptName, callback) {
+  var scriptEl = document.createElement("script");
+  scriptEl.src = chrome.extension.getURL("dist/" + scriptName + ".js");
+  scriptEl.addEventListener("load", callback, false);
+  document.head.appendChild(scriptEl);
+}
+
+loadScript("index.bundle", function () {
+    console.log("Loaded index.bundle.js");
+});
